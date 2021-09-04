90d07d22c2ff892cc00745b047b27cf60bdebfc0
dmarenin <39666107+dmarenin@users.noreply.github.com>
2021-09-04 10:51:37 +0500

    Update price.py

--- BollingerBandsBot/price.py
+++ BollingerBandsBot/price.py
@@ -19,14 +19,12 @@ def get_data(qpProvider):
 
     data = data['data']
     for x in data:
-        #d['datetime'] = x['datetime']
-
-        #x['datetime'] = datetime.datetime(x['datetime']['year'],
-        #                                  x['datetime']['month'],
-        #                                  x['datetime']['day'],
-        #                                  x['datetime']['hour'],
-        #                                  x['datetime']['min'],
-        #                                  x['datetime']['sec'])
+        x['datetime'] = datetime.datetime(x['datetime']['year'],
+                                          x['datetime']['month'],
+                                          x['datetime']['day'],
+                                          x['datetime']['hour'],
+                                          x['datetime']['min'],
+                                          x['datetime']['sec'])
 
         x['close'] = int(x['close'])
         x['high'] = int(x['high'])
