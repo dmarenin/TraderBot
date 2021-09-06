718e1677faf2b2bd79637b7dd7b2ae89c7200fea
dmarenin <39666107+dmarenin@users.noreply.github.com>
2021-09-06 20:19:09 +0500

    Update main.py

--- BollingerBandsBot/main.py
+++ BollingerBandsBot/main.py
@@ -5,6 +5,8 @@ import bb
 import message
 import db
 import offers
+import datetime
+import volume
 
 
 #bid покупка
@@ -22,13 +24,17 @@ QUOTES = []
 def on_trans_reply(data):
     db.log('OnTransReply', data['data'])
 
+    offers.update_on_trans_reply(data['data'])
+
 def on_order(data):
     db.log('OnOrder', data['data'])
 
-    offers.update(data['data'])
+    offers.update_on_order(data['data'])
 
 def on_trade(data):
     db.log('OnTrade', data['data'])
+
+    offers.update_on_trade(data['data'])
 
 def on_futures_client_holding(data):
     db.log('OnFuturesClientHolding', data['data'])
@@ -49,11 +55,36 @@ def do_loop(qpProvider):
     global QUOTES
 
     while True:
-        time.sleep(1)
+        time.sleep(2)
         while not message.TRADE:
             time.sleep(5)
 
         start_time = time.time()
+        try:
+            _quotes = QUOTES.copy()
+        except:
+            continue
+
+        #orders = qpProvider.GetAllOrders()['data']
+
+        balance = 0
+
+        futures_holdings = qpProvider.GetFuturesHoldings()
+        if not futures_holdings is None:
+            futures_holdings = futures_holdings['data']
+            for f in futures_holdings:
+                if f['sec_code']!=secCode:
+                    continue
+                balance = f['todaybuy']-f['todaysell']
+                break
+
+        db.log('quotes', _quotes)
+
+        if len(_quotes)==0:
+            continue
+
+        if _quotes.get('bid') is None:
+            continue
 
         bb_data = bb.get_data(qpProvider)
         db.log('bb_data', bb_data)
@@ -61,39 +92,54 @@ def do_loop(qpProvider):
         price_data = price.get_data(qpProvider)
         db.log('price_data', price_data)
 
+        #volume_data = volume.get_data(qpProvider)
+        #db.log('volume_data', volume_data)
+
         last_bb_data = bb_data[-1]
         last_price_data = price_data[-1]
 
         last_bb_data_close = bb_data[-2]
         last_price_data_close = price_data[-2]
 
-        try:
-            _quotes = QUOTES.copy()
-        except:
-            continue
+        last_bid = int(_quotes['bid'][-1]['price'])
+        db.log('last_bid', last_bid)
 
-        db.log('quotes', _quotes)
+        first_offer = int(_quotes['offer'][0]['price'])
+        db.log('first_offer', first_offer)
 
-        #if len(_quotes)==0:
-        #    continue
+        step1 = round((time.time() - start_time), 4)
 
-        #if _quotes.get('bid') is None:
-        #    continue
+        db.log('step1', step1)
 
-        #last_bid = int(_quotes['bid'][-1]['price'])
-        #db.log('last_bid', last_bid)
+        if balance<0:
+            #Открыт шорт
+            pass
+        elif balance>0:
+            #Открыт лонг
+            pass
+        else:
+            #Нет открытых позиций
+            pass
 
-        #first_offer = int(_quotes['offer'][0]['price'])
-        #db.log('first_offer', first_offer)
 
-        take = offers.get_take()
+        if first_offer>last_bb_data_close['medium_line']:
+            take = offers.get_take('short')
+            if last_bb_data_close['upper_line']<=last_bid and balance==0:
+                offers.add_offer(qpProvider, last_bb_data['upper_line'], last_bid, 'S', bb_data, price_data, _quotes, account, classCode, secCode, balance, 'short')
 
-        ####
-        first_offer = 180000+200
+            elif take>0 and balance!=0:
+                if take>=first_offer:
+                    offers.add_offer(qpProvider, take, first_offer, 'B', bb_data, price_data, _quotes, account, classCode, secCode, balance, 'short')
 
-        offers.add_offer(qpProvider, last_bb_data['lower_line'], first_offer, 'B', bb_data, price_data, _quotes, account, classCode, secCode)
+        else:
+            take = offers.get_take('long')
 
-        offers.add_offer(qpProvider, take, take, 'S', bb_data, price_data, _quotes, account, classCode, secCode)
+            if last_bb_data_close['lower_line']>=first_offer and balance==0:
+                offers.add_offer(qpProvider, last_bb_data['lower_line'], first_offer, 'B', bb_data, price_data, _quotes, account, classCode, secCode, balance, 'long')
+
+            elif take>0 and balance!=0:
+                if take<=last_bid:
+                    offers.add_offer(qpProvider, take, last_bid, 'S', bb_data, price_data, _quotes, account, classCode, secCode, balance, 'long')
 
         offers.garbage_collect(qpProvider, account, classCode, secCode)
 
@@ -101,35 +147,8 @@ def do_loop(qpProvider):
         #    offers.close_all()
         #    message.CLOSE_ALL = False
 
-        #offers.get_offers()
-
-        step1 = round((time.time() - start_time), 4)
-
-        print(f"--- {step1}s seconds step1 ---" )
-        print('')
-
-        db.log('step1', step1)
-
-        continue
-        ####
-
-        if last_bb_data_close['lower_line']>=first_offer:
-            offers.add_offer(qpProvider, last_bb_data['lower_line'], first_offer, 'B', bb_data, price_data, _quotes, account, classCode, secCode)
-
-        elif take>0:
-            if take<=last_bid:
-                offers.add_offer(qpProvider, last_bid, take, 'S', bb_data, price_data, _quotes, account, classCode, secCode)
-
-        offers.garbage_collect()
-
-        #if message.CLOSE_ALL:
-        #    offers.close_all()
-        #    message.CLOSE_ALL = False
-
         step2 = round((time.time() - start_time), 4)
 
-        print(f"--- {step2}s seconds step1 ---" )
-        print('')
         db.log('step2', step2)
 
     pass
@@ -144,7 +163,7 @@ def main():
     qpProvider.OnTransReply = on_trans_reply
     qpProvider.OnOrder = on_order
     qpProvider.OnTrade = on_trade
-    qpProvider.OnFuturesClientHolding = on_futures_client_holding
+    #qpProvider.OnFuturesClientHolding = on_futures_client_holding
     qpProvider.OnDepoLimit = on_depo_limit
     qpProvider.OnDepoLimitDelete = on_depo_limit_delete
 
